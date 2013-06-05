60819b52afac2db71ee56f08e52701bd65680815
Sietse de Kaper <sietse@lunatech.com>
2013-06-05 10:33:51 +0200

    Chapter 5 sample code

---
+++ README.md
@@ -0,0 +1,4 @@
+Chapter 5: Controllers
+====================================
+
+This is the sample code for chapter five of [Play for Java](http://bit.ly/playjava).
---
+++ app/controllers/Products.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import models.Product;
+import play.data.Form;
+import play.mvc.Result;
+import play.mvc.Controller;
+import play.mvc.With;
+import views.html.products.*;
+
+import java.util.Set;
+
+@With(CatchAction.class)
+public class Products extends Controller {
+
+  private static final Form<Product> productForm = Form.form(Product.class);
+
+  public static Result index() {
+    return redirect(routes.Products.list(1));
+  }
+
+  public static Result list(Integer page) {
+    Set<Product> products = Product.findAll();
+    return ok(list.render(products));
+  }
+
+  public static Result newProduct() {
+    return ok(details.render(productForm));
+  }
+
+  public static Result details(String ean) {
+    final Product product = Product.findByEan(ean);
+    if (product == null) {
+      return notFound(String.format("Product %s does not exist.", ean));
+    }
+
+    Form<Product> filledForm = productForm.fill(product);
+    return ok(details.render(filledForm));
+  }
+
+  public static Result save() {
+    Form<Product> boundForm = productForm.bindFromRequest();
+    if(boundForm.hasErrors()) {
+      flash("error", "Please correct the form below.");
+      return badRequest(details.render(boundForm));
+    }
+
+    Product product = boundForm.get();
+    product.save();
+    flash("success",
+        String.format("Successfully added product %s", product));
+
+    return redirect(routes.Products.list(1));
+  }
+}
